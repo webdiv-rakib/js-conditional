54b850a334837d35763d7218852b5f6057613df4
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-30 04:45:55 +0600

    nested condition example

---
+++ nested.js
@@ -0,0 +1,20 @@
+const money = 10;
+
+if (money > 300){
+    console.log("Bro you are rich");
+}
+
+else{
+    if(money > 100){
+        console.log("Bro not poor nor rich");
+    }
+    else{
+        if (money > 0 ){
+            console.log("Bro kola kha");
+        }
+        else{
+            console.log("Tui mara kha");
+        }
+    }
+
+}
