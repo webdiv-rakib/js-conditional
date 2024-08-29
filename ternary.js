addd9b35eebe34acdef4d70896bc60079eb0a2c3
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-30 04:57:42 +0600

    ternary example

---
+++ ternary.js
@@ -0,0 +1,17 @@
+/**
+ * Ternary --> Three parts
+ * 
+ *      ?      :
+ *  condition ? do something when true : do something when false 
+ */
+
+const age = 18;
+
+// if (age >=18){
+//     console.log("You can vote.");
+// }
+// else {
+//     console.log("You can't vote.");
+// }
+
+age >= 18 ? console.log("Vote Dio") : console.log("Vote dite parba na");
