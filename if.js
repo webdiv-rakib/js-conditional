850b9f0bae0c95648554b1d509487f4d73328c47
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-29 22:44:13 +0600

    if else statement

---
+++ if.js
@@ -0,0 +1,58 @@
+/**
+ * Tui jodi amake khawash taile ami ashbo
+ * 
+ * if you buy food for me i will come
+ * 
+ * --------------------
+ * if briyani price less than 300take
+ *    i will buy
+ * 
+ * -----------Structure-------------
+ * 
+ * if (condition){
+ * 
+ * }
+ * 
+ * --------------------
+ * if (condition){
+ * 
+ * }
+ * 
+ */
+
+// if (3 < 10) {
+//     console.log('i have a smaller value');
+// }
+
+// var biriyaniPrice = 250;
+
+// if (biriyaniPrice < 300) {
+//     console.log('Mama give me 1packet of biriyani');
+// }
+
+/**
+ * if (condition){
+ *      execute code if condition above is true
+ * }
+ * else {
+ *      execute code if condition is false
+ * }
+ */
+
+// var weight = 10;
+
+// if (weight < 20) {
+//     console.log('I will bring it on my own');
+// }
+// else {
+//     console.log('I will rent a rickshaw');
+// }
+
+var pantPrice = 700;
+
+if (pantPrice < 600){
+    console.log("I will buy pant");
+}
+else {
+    console.log("I will but half pant")
+}
