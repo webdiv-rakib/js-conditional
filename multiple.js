155fb1bbff154b82dfd1e1b72275f87e7d4b005b
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-30 03:36:59 +0600

    multiple condition

---
+++ multiple.js
@@ -0,0 +1,42 @@
+const salary = 25000;
+const isBCS = true;
+const height = 70;
+const hasCar = false;
+
+// if (salary > 20000 && height > 50){
+//     console.log("Suuu------Patro");
+// }
+// else{
+//     console.log("onno patro khuji");
+// }
+
+// if (salary > 26000 || height > 71 ){
+//     console.log("Eso baba kobul");
+// }
+// else{
+//     console.log("Tmi tata bye bye");
+// }
+
+// More and More condition
+// if (salary > 26000 || height > 71 || isBCS == true ){
+//     console.log("Eso baba kobul");
+// }
+// else{
+//     console.log("Tmi tata bye bye");
+// }
+
+
+// if (salary > 26000 && height > 71 && isBCS == true ){
+//     console.log("Eso baba kobul");
+// }
+// else{
+//     console.log("Tmi tata bye bye");
+// }
+
+// --------------Complex Condition-------------
+if ((salary > 21000 && hasCar == true) || isBCS == true ){
+    console.log('Alhamdulillah Kobul');
+}
+else{
+    console.log('Tmi ashte paro');
+}
