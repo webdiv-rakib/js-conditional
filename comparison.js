cc406d1aef3808b9d2f36fae7fcf5085ec0b49e3
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-29 21:21:05 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,25 @@
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 == 10);
+// console.log(5 != 10);
+// console.log(5 >= 10);
+// console.log(15 <= 10);
+
+// --------------------------
+var peyaraPrice = 40;
+var applePrice = 250;
+
+// console.log(peyaraPrice > applePrice);
+// console.log(peyaraPrice < applePrice);
+
+/**
+ * ==
+ * ===
+ */
+
+// console.log(10 == '10');
+// console.log(10 === '10');
+
+// ------------------------
+console.log( 10 != '10');
+console.log( 10 !== '10');
