0b8711f1083d4769b81e9dfad3e720b6c176df5b
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-29 20:59:11 +0600

    previous overwiew

---
+++ variable.js
@@ -0,0 +1,32 @@
+/**
+ * Variable ==> container
+ */
+// number
+var sunglassPrice = 80;
+
+// string
+var subject = 'CSE';
+
+// boolean
+var passed = true;
+
+/**
+ * Naming Convention
+ * 1. single word
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. can not start with number
+ * 5. can not be a keyword
+ * 6. prefer camel case
+ */
+
+var lastExamResult = 89;
+
+/**
+ * Operations
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=
+ * 3. isNaN
+ * 4. null
+ * 5.Number
+ */
