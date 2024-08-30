7161944587492de3cf175c02d0b302469f4ca116
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-30 17:00:46 +0600

    problem 1

---
+++ home-work.js
@@ -0,0 +1,13 @@
+/**
+ * Free Drinks
+ *      - Burger more than 500 tk: free Coke
+ *      - Else Coke: 30tk
+ */
+
+const burgerPrice = 400;
+if (burgerPrice >= 500){
+    console.log("Free Coke");
+}
+else{
+    console.log("Coke: 30tk");
+}
