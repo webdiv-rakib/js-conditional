586654029c5665fad7980c000aaa9ed63e9e2821
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-30 17:44:50 +0600

    problem-5

---
+++ problem-5.js
@@ -0,0 +1,26 @@
+/***
+
+you have two numbers in two variables, called: num1, num2
+
+now declare a variable called result. 
+if num1 is bigger than num2 then result will be double of num1. if not, then the value of the variable result will be the sum of num1 and num2.
+
+write a simple if-else. 
+
+also, write it using ternary operator.
+
+ */
+
+let num1 = 20;
+let num2 = 25;
+let result = num1 + num2;
+
+if (num1 > num2) {
+    let result = num1 * 2;
+    console.log(result);
+}
+else {
+    console.log(result);
+}
+
+
